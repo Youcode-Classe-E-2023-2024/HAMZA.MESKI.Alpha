22a3a42708163e19812097157a133ff5d3131487
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2024-01-03 16:18:12 +0100

    start

---
+++ app/config/config.php
@@ -0,0 +1,16 @@
+<?php 
+// database params 
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'shareposts');
+
+// app root 
+define('APPROOT', dirname(dirname(__FILE__)));
+// URL root 
+define('URLROOT', 'http://localhost/HAMZA.MESKI.Alpha');
+// site name
+define('SITENAME', 'XENO');
+// app version 
+define('APPVERSION', '1.0.0');
+?>
